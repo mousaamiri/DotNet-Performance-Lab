6614f6674c15669716a635b8fa991d412102a5aa
mousaamiri <106370749+mousaamiri@users.noreply.github.com>
2026-08-06 23:56:51 +0330

    Add CacheStampede.Demo project to simulate Cache Stampede phenomenon with Redis caching

---
+++ src/04-CacheStampede.Demo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using _04_CacheStampede.Demo.Model;
+using _04_CacheStampede.Demo.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.InputEncoding = System.Text.Encoding.UTF8;
+
+var services = new ServiceCollection();
+services.AddScoped<IDataService, DataService>();
+services.AddStackExchangeRedisCache(op =>
+{
+    op.Configuration = "localhost:6379";
+    op.InstanceName = "04-CacheStampede.Demo:";
+});
+var dataService = services.BuildServiceProvider().GetRequiredService<IDataService>();
+
+
+List<Func<int, Task<Product?>>> tasks = [];
+
+for (var i = 0; i < 50; i++)
+{
+    tasks.Add(async id=>await dataService.GetProductAsync(id));
+}
+
+while (true)
+{
+    var sw = new Stopwatch();
+    sw.Start();
+    await Task.WhenAll(tasks.Select(async task =>await task(1)));
+    sw.Stop();
+
+    Console.WriteLine("=====================================================");
+    Console.WriteLine($"The entire operation was completed in {sw.ElapsedMilliseconds} ms");
+
+    
+    Console.WriteLine("=====================================================");
+    Console.Write("Do you want to try again ? (y/n)");
+    var input = Console.ReadLine();
+    Console.Write("");
+    Console.WriteLine("=====================================================");
+    if(input == "y")
+        continue;
+    else break;
+}
---
+++ src/04-CacheStampede.Demo/Services/GetDataService.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Concurrent;
+using System.Text.Json;
+using _04_CacheStampede.Demo.Model;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace _04_CacheStampede.Demo.Services;
+
+public interface IDataService
+{
+    Task<Product?> GetProductAsync(int id);
+}
+public class DataService(IDistributedCache cache) : IDataService
+{
+    private readonly string _dataSourceUrl = Path.Combine(AppContext.BaseDirectory, "Data", "products.json");
+    private static int _dbRefCounter = 0;
+    private static readonly ConcurrentDictionary<string, SemaphoreSlim> KeyLocks = [];
+    private readonly JsonSerializerOptions _serializerOptions = new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive = true
+    };
+    private async Task<List<Product>> GetProductsAsync()
+    {
+        await Task.Delay(2000);
+        var json = await File.ReadAllTextAsync(_dataSourceUrl);
+        var product = JsonSerializer.Deserialize<List<Product>>(json, _serializerOptions) ?? [];
+
+        return product;
+    }
+
+    public async Task<Product?> GetProductAsync(int id)
+    {
+        var key = $"1product:{id}";
+        var product = await GetFromCache(key);
+        if (product is not null) return product;
+
+
+        var keyLock = KeyLocks.GetOrAdd(key, _ => new SemaphoreSlim(1, 2));
+        await keyLock.WaitAsync();
+        
+        try
+        {
+            product = await GetFromCache(key);
+            if (product is not null) return product;
+
+            await Task.Delay(2000);
+            var products = await GetProductsAsync();
+            product = products.FirstOrDefault(p => p.Id == id);
+            if (product is not null)
+            {
+                await cache.SetStringAsync(key, JsonSerializer.Serialize(product), new DistributedCacheEntryOptions
+                {
+                    SlidingExpiration = TimeSpan.FromSeconds(10)
+                });
+            }
+
+            Interlocked.Increment(ref _dbRefCounter);
+            Console.WriteLine(
+                $"Product: {product?.Name}, Price: {product?.Price} , -- ({_dbRefCounter}th reference to the database)");
+            return product;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            throw;
+        }
+        finally
+        {
+            keyLock.Release();
+        }
+    }
+
+    private async Task<Product?> GetFromCache(string key)
+    {
+        var jsonCached = await cache.GetStringAsync(key);
+        if (jsonCached != null)
+        {
+            var cachedProduct = JsonSerializer.Deserialize<Product>(jsonCached, _serializerOptions);
+            Console.WriteLine($"Product: {cachedProduct?.Name}, Price: {cachedProduct?.Price} , -- (Cache (Hit))");
+            return cachedProduct;
+        }
+
+        return null;
+    }
+
+   
+}
